82afe379b0a5a7e38d3466d74df6b98001cd336f
Denitsa Ruseva <russeva3@gmail.com>
2020-10-07 16:13:37 +0300

    Category model and db migration created

---
+++ src/Data/MadHoneyStore.Data.Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace MadHoneyStore.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+    }
+}
